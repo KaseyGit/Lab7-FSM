992f8f5df0a03e3d32f776751b52b6359a1ade92
KaseyGit <kaw327@lehigh.edu>
2025-10-21 11:03:03 -0400

    Add files via upload

---
+++ 4095counter.v
@@ -0,0 +1,25 @@
+module counter(
+	input wire clk, 
+	input wire reset, 
+	output reg [11:0] count //12 bit counter output
+);
+
+reg[11:0] next_count; //next state of the counter
+
+//get the next count
+always @(*) begin
+	next_count = count + 1;
+end
+
+always @(posedge clk) begin
+	if(reset) begin
+	count <= 12'd0; //reset output to 0
+		end
+	else begin
+	count <= next_count; //update the counter
+	end
+end
+
+endmodule
+
+module counter_tb();
