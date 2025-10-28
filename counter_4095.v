8e75a70d5a3a762e4624af841610351e68efa525
sophia-pham <slp227@lehigh.edu>
2025-10-28 15:26:15 -0400

    Update counter_4095.v

--- counter_4095.v
+++ counter_4095.v
@@ -4,21 +4,20 @@ module counter_4095(
 	output reg [11:0] count //12 bit counter output
 );
 
-reg[11:0] next_count; //next state of the counter
-
-//get the next count
-always @(*) begin
-	next_count = count + 1;
-end
 
 always @(posedge clk) begin
 	if(reset) begin
 	count <= 12'd0; //reset output to 0
 		end
-	else begin
-	count <= next_count; //update the counter
+	else if(count == 12'd4095)begin
+	   count <= 12'd0;
 	end
+	else begin
+	count <= count + 1; //update the counter
+	end
+
 end
 
 endmodule
+
 
