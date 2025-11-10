1428269940a4465c2dee534a5a6acd2d2c643b05
KaseyGit <kaw327@lehigh.edu>
2025-11-09 19:59:54 -0500

    Update Clock_divider.v

--- Clock_divider.v
+++ Clock_divider.v
@@ -1,19 +1,32 @@
 `timescale 1ns / 1ps
-//THIS IS THE CLOCK DIVIDER MODULE FROM LAB 6
-module Clock_divider(clock_in,clock_out);
+//clock divider 50MHz to 100Hz
+module Clock_divider(
+input clock_in,
+input reset, 
+output reg clock_out);
 
-input clock_in;
-output reg clock_out = 1'b0;
+//19 bit counter to count to 500,000
+reg[18:0] counter;
 
-reg[1:0] counter=2'd0;
+//50,000,000 / (100 Hz * 2) = 250,000
+localparam MAX_COUNT = 19'd9;
 
- always @(posedge clock_in) begin
- 	counter <= counter + 1;
- 	if (counter == 2'b01) begin
- 	clock_out <= ~clock_out; // Toggle the output clock
- 		counter <= 0;
+
+ always @(posedge clock_in or posedge reset) begin
+    if (reset) begin
+        //reset to 0
+        counter <= 19'd0;
+        clock_out <= 1'b0;
+    end
+    else begin
+        if(counter == MAX_COUNT) begin
+            counter <= 19'b0;
+            clock_out <= ~clock_out;
+        end
+        else begin
+        counter <= counter + 1;
+        end
     end
  end
  endmodule
-
 
