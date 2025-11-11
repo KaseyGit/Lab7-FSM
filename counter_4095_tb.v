ab2472b914a46dbb7aa3caa42a30ee9ae574d038
KaseyGit <kaw327@lehigh.edu>
2025-11-11 16:09:04 -0500

    Update counter_4095_tb.v
    For the new ports in counter_4095

--- counter_4095_tb.v
+++ counter_4095_tb.v
@@ -1,14 +1,14 @@
 module counter_4095_tb();
 
-reg clk, reset;
-wire count;
+reg clk, reset, en;
+wire count, rdy;
 
-counter_4095 dut(.clk(clk), .reset(reset), .count(count));
+	counter_4095 dut(.clk(clk), .reset(reset), .count(count), .en(en), .rdy(rdy));
 
 always #40950 clk = ~clk;
 	
 initial begin 
-	clk = 0; reset = 1;
+	clk = 0; reset = 1; en = 1;
 	#15;
 	
 	reset = 0;
