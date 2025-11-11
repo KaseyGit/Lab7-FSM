a4e05c32e18764a834d4403b755a0ff3e4ac9750
KaseyGit <kaw327@lehigh.edu>
2025-11-11 16:05:18 -0500

    Update counter_4095.v

--- counter_4095.v
+++ counter_4095.v
@@ -1,25 +1,26 @@
 module counter_4095(
+    input wire en,
 	input wire clk, 
 	input wire reset, 
-	output reg [11:0] count //12 bit counter output
-);
+	output reg [11:0] count, //12 bit counter output
+	output reg rdy);
 
-//use asynch reset
+
 always @(posedge clk or posedge reset) begin
+    rdy <= 0;
 	if(reset) begin
-	count <= 12'd0; //reset output to 0
-		end
+        count <= 12'd0; //reset output to 0
+        rdy <= 0;
+	end
 	else if(count == 12'd4095)begin
 	   count <= 12'd0;
 	end
-	else begin
-	count <= count + 1; //update the counter
+	else if (en) begin
+        count <= count + 1; //update the counter
+        rdy <= 1; //enable into the binary to BCD module
 	end
 
 end
 
 endmodule
-
-
-
 
