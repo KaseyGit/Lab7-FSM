d32dbfbbdfeb18c058fb1a817c5e77ba64b5c890
KaseyGit <kaw327@lehigh.edu>
2025-11-11 16:04:22 -0500

    Update bin2bcd.v

--- bin2bcd.v
+++ bin2bcd.v
@@ -1,10 +1,10 @@
-module bin2bcd(bin, en, clk, reset, bcd_out, rdy); //FSM version
+module bin2bcd(bin, en, clk,reset,  bcd_out, rdy); //FSM version
 	input [11:0] bin;
-	input reset; //added a reset
 	input clk;
 	input en;
 	output [15:0] bcd_out;
 	output rdy;
+	input reset;
 	
 	parameter IDLE = 3'd0, SETUP = 3'd1, ADD = 3'd2, SHIFT = 3'd3, DONE = 3'd4;
 	
@@ -17,15 +17,15 @@ module bin2bcd(bin, en, clk, reset, bcd_out, rdy); //FSM version
 	
 	//behavior based on flowchart in slides
 	always @(posedge clk or posedge reset) begin
-	   if(reset) begin
+	   if(reset)begin
 	       busy <= 1'b0;
 	       bcd_data <= 28'd0;
-	       state <= IDLE;
+	       state <= SETUP;
 	       sh_counter <= 4'd0;
 	       add_counter <= 3'd0;
 	       result_rdy <= 1'b0;
-	    end
-        else begin	
+	   end
+	   else begin
 		case (state)
 			IDLE:
 				begin
@@ -37,7 +37,7 @@ module bin2bcd(bin, en, clk, reset, bcd_out, rdy); //FSM version
 			
 			SETUP:
 				begin
-				    bcd_data <= {16'd0, bin};
+				    bcd_data = bin;
 					busy <= 1;
 					state <= ADD;
 				end
@@ -48,28 +48,21 @@ module bin2bcd(bin, en, clk, reset, bcd_out, rdy); //FSM version
 						0: //time to check the ones place
 						begin
 							if (bcd_data[15:12] > 4) begin //if first 4 bits of BCD are >4
-								bcd_data[15:12] <= bcd_data[15:12] + 3; //ADD 3 to the BCD number starting from bit position 12
-								state <= SHIFT;
-								add_counter <= 3;
+								bcd_data[27:12] <= bcd_data[27:12] + 3; //ADD 3 to the BCD number starting from bit position 12
 							end
-							state <= SHIFT;
 							add_counter <= add_counter + 1;
 						end
 						1: //check the tens place
 						begin
 							if (bcd_data[19:16] > 4) begin
-								bcd_data[19:16] <= bcd_data[19:16] + 3;
-								state <= SHIFT;
-								add_counter <= 3;
+								bcd_data[27:16] <= bcd_data[27:16] + 3;
 							end
 							add_counter <= add_counter + 1;
 						end
 						2: //check the hundreds place
 						begin
 							if (bcd_data[23:20] > 4) begin
-								bcd_data[23:20] <= bcd_data[23:20] + 3;
-								state <= SHIFT;
-								add_counter <= 3;
+								bcd_data[27:20] <= bcd_data[27:20] + 3;
 							end
 							add_counter <= add_counter + 1;
 						end
@@ -77,8 +70,6 @@ module bin2bcd(bin, en, clk, reset, bcd_out, rdy); //FSM version
 						begin
 							if (bcd_data[27:24] > 4) begin
 								bcd_data[27:24] <= bcd_data[27:24] + 3;
-							    state <= SHIFT; 
-							    add_counter <= 3;
 							end
 							add_counter <= 0;
 							state <= SHIFT;
@@ -98,18 +89,12 @@ module bin2bcd(bin, en, clk, reset, bcd_out, rdy); //FSM version
 			DONE:
 				begin
 					state <= IDLE;
-					result_rdy <= 1;
+					result_rdy = 1;
 				end
 			default: state <= IDLE;
 		endcase
 	end
 	end
-	
 	assign bcd_out = bcd_data[27:12];
 	assign rdy = result_rdy;
 endmodule
-
-	assign bcd_out = bcd_data[27:12];
-	assign rdy = result_rdy;
-endmodule
-
