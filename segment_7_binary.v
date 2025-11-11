68a3d3c1fc1b732465c6ceb3744a7aea7b49ed5c
KaseyGit <kaw327@lehigh.edu>
2025-11-11 16:05:34 -0500

    Update segment_7_binary.v

--- segment_7_binary.v
+++ segment_7_binary.v
@@ -1,3 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/16/2025 01:38:41 PM
+// Design Name: 
+// Module Name: segment_7_binary
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module segment_7_binary(
     input [3:0] data_in,
     output reg [6:0] seg
